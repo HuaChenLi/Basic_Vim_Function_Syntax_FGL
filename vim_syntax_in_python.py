5e8c88670912d0b3a5a336cb0bc75cee552ecdc3
HuaChenLi <huachenli30@gmail.com>
2024-09-05 18:36:41 +1000

    Fixed " not being tokenized properly sometimes

--- vim_syntax_in_python.py
+++ vim_syntax_in_python.py
@@ -210,7 +210,7 @@ def tokenizeString(inputString):
     # basically, the massive line of regex code repeats, so we will grab all printable characters (since all printable characters are between ! to ~ except white spaces)
     # the repeating section contains all the special characters in Genero
     # probably can create a regex that is smart enough to do the whole thing by itself, but can probably just handle it in the python code afterwards
-    tokenBlock = re.findall(r"(?:(?!\.|,|'|`|\"\||\(|\)|#|{|}|\[|\]|<|>|-|!|$|\\|\n)[!-~])+|\.|,|'|`|\"\||\(|\)|#|{|}|\[|\]|<|>|-|!|$|\\|\n", inputString)
+    tokenBlock = re.findall(r"(?:(?!\.|,|'|`|\"|\||\(|\)|#|{|}|\[|\]|<|>|-|!|$|\\|\n)[!-~])+|\.|,|'|`|\"|\||\(|\)|#|{|}|\[|\]|<|>|-|!|$|\\|\n", inputString)
     return tokenBlock
 
 
