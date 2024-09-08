b6d2e7ff2d45093e2c776112d2a9b2f1d5429042
Hua Chen Li <huachenli30@gmail.com>
2024-09-08 23:07:25 +1000

    Added some basic unit tests Can add more later when I want

---
+++ vim_syntax_unit_tests.py
@@ -0,0 +1,23 @@
+import unittest
+import vim_syntax_in_python
+
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_tokenizeString(self):
+        inputString = "jk,89"
+        outputArray = ["jk", ",", "89", ""]
+        self.assertEqual(vim_syntax_in_python.tokenizeString(inputString), outputArray)
+
+        inputString = "import fgl function"
+        outputArray = ["import", "fgl", "function", ""]
+        self.assertEqual(vim_syntax_in_python.tokenizeString(inputString), outputArray)
+
+        inputString = "\"This is a string"
+        outputArray = ["\"", "This", "is", "a", "string", ""]
+        self.assertEqual(vim_syntax_in_python.tokenizeString(inputString), outputArray)
+
+if __name__ == '__main__':
+    unittest.main()
+
