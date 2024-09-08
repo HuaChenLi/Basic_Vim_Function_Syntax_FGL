79d693a63d23f6c9153082c971c2a635a6f2d8a3
Hua Chen Li <huachenli30@gmail.com>
2024-09-08 23:59:07 +1000

    Added unit test for the tokenizeLinesOfFiles() function

--- vim_syntax_unit_tests.py
+++ vim_syntax_unit_tests.py
@@ -18,6 +18,11 @@ class TestStringMethods(unittest.TestCase):
         outputArray = ["\"", "This", "is", "a", "string", ""]
         self.assertEqual(vim_syntax_in_python.tokenizeString(inputString), outputArray)
 
+    def test_tokenizeLinesOfFiles(self):
+        inputString = [("import fgl import.basic.functions"), ("import fgl other.functions")]
+        outputArray = [("import", 1), ("fgl", 1), ("import", 1), (".", 1), ("basic", 1), (".", 1), ("functions", 1), ("", 1), ("\n", 1), ("import", 2), ("fgl", 2), ("other", 2), (".", 2), ("functions", 2), ("", 2), ("\n", 2)]
+        self.assertEqual(vim_syntax_in_python.tokenizeLinesOfFiles(inputString), outputArray)
+
 if __name__ == '__main__':
     unittest.main()
 
