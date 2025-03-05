db441ccab9415138675a2b47337c42609d1f6065
HuaChenLi <huachenli30@gmail.com>
2025-03-05 20:44:07 +1100

    fixed unit test

--- vim_syntax_unit_tests.py
+++ vim_syntax_unit_tests.py
@@ -1,5 +1,6 @@
 import unittest
-import vim_syntax_in_python
+
+import lib.tokenize
 
 
 
@@ -8,23 +9,23 @@ class TestStringMethods(unittest.TestCase):
     def test_tokenizeString(self):
         inputString = "jk,89"
         outputArray = ["jk", ",", "89"]
-        self.assertEqual(vim_syntax_in_python.tokenizeString(inputString), outputArray)
+        self.assertEqual(lib.tokenize.tokenizeString(inputString), outputArray)
 
         inputString = "import fgl function"
         outputArray = ["import", "fgl", "function"]
-        self.assertEqual(vim_syntax_in_python.tokenizeString(inputString), outputArray)
+        self.assertEqual(lib.tokenize.tokenizeString(inputString), outputArray)
 
         inputString = "\"This is a string"
         outputArray = ["\"", "This", "is", "a", "string"]
-        self.assertEqual(vim_syntax_in_python.tokenizeString(inputString), outputArray)
+        self.assertEqual(lib.tokenize.tokenizeString(inputString), outputArray)
 
         inputString = "-- string yeah comment"
         outputArray = ["--", "string", "yeah", "comment"]
-        self.assertEqual(vim_syntax_in_python.tokenizeString(inputString), outputArray)
+        self.assertEqual(lib.tokenize.tokenizeString(inputString), outputArray)
 
         inputString = "\\\\\\r whacko escapes \""
         outputArray = ["\\\\\\", "r", "whacko", "escapes", "\""]
-        self.assertEqual(vim_syntax_in_python.tokenizeString(inputString), outputArray)
+        self.assertEqual(lib.tokenize.tokenizeString(inputString), outputArray)
 
 if __name__ == '__main__':
     unittest.main()
