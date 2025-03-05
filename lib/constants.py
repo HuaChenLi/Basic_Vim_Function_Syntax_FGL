2e249a86a594eb93b83a15fc910d664bc0c2cf5f
HuaChenLi <huachenli30@gmail.com>
2025-03-05 23:26:09 +1100

    moved the genero key words into the library

--- lib/constants.py
+++ lib/constants.py
@@ -13,7 +13,9 @@ FGL_SUFFIX = ".4gl"
 
 GENERO_KEY_WORDS = set()
 
-KEYWORDS_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "genero_key_words.txt")
+LIB_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
+
+KEYWORDS_FILE = os.path.join(LIB_DIR, "genero_key_words.txt")
 
 if os.path.isfile(KEYWORDS_FILE):
     GENERO_KEY_WORDS.update(open(KEYWORDS_FILE, "r").read().lower().split("\n"))
