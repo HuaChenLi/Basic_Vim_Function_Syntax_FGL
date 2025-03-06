38bbf70506bd212e01c055020063a2caa06fa654
HuaChenLi <huachenli30@gmail.com>
2025-03-06 22:13:47 +1100

    separated the test directories

--- vim_syntax_unit_tests.py
+++ vim_syntax_unit_tests.py
@@ -38,7 +38,8 @@ class TestStringMethods(unittest.TestCase):
 
 class TestFileSearches(unittest.TestCase):
     PACKAGE_FILE_TEST_DIRECTORY = os.path.join(UNIT_TEST_DIRECTORY, "getPackageFile")
-    FIND_FUNCTION_FROM_SPECIFIC_LIBRARY = os.path.join(UNIT_TEST_DIRECTORY, "findFunctionFromSpecificLibrary")
+    FIND_FUNCTION_FROM_SPECIFIC_LIBRARY_DIRECTORY = os.path.join(UNIT_TEST_DIRECTORY, "findFunctionFromSpecificLibrary")
+    FIND_FUNCTION_FROM_MAKEFILE_DIRECTORY = os.path.join(UNIT_TEST_DIRECTORY, "findFunctionFromMakefile")
 
     def test_getPackageFile(self):
         libPath = os.path.join(self.PACKAGE_FILE_TEST_DIRECTORY, "testLib")
@@ -46,14 +47,14 @@ class TestFileSearches(unittest.TestCase):
 
     def test_findFunctionFromSpecificLibrary(self):
         # find basic function
-        libPath = os.path.join(self.FIND_FUNCTION_FROM_SPECIFIC_LIBRARY, "testLib")
+        libPath = os.path.join(self.FIND_FUNCTION_FROM_SPECIFIC_LIBRARY_DIRECTORY, "testLib")
         self.assertEqual( findGeneroObject.findFunctionFromSpecificLibrary("test3.4gl", [libPath], "test_function"), (str(os.path.join(libPath, "test3.4gl")), 5) )
 
     def test_findFunctionFromMakefile(self):
         # find basic function
-        libPath = os.path.join(self.PACKAGE_FILE_TEST_DIRECTORY, "testLib")
+        libPath = os.path.join(self.FIND_FUNCTION_FROM_MAKEFILE_DIRECTORY, "testLib")
         os.environ['FGLLDPATH'] = libPath
-        self.assertEqual( findGeneroObject.findFunctionFromMakefile(self.PACKAGE_FILE_TEST_DIRECTORY, "test_function400"), (str(os.path.join(libPath, "test4.4gl")), 6) )
+        self.assertEqual( findGeneroObject.findFunctionFromMakefile(self.FIND_FUNCTION_FROM_MAKEFILE_DIRECTORY, "test_function400"), (str(os.path.join(libPath, "test4.4gl")), 6) )
 
     def test_findFunctionAndMethods(self):
         pass
